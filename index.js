278efca25435c307820c8beb58c8efff796dab09
Prince <avecillasprince@yahoo.com>
2021-06-28 19:01:39 -0400

    Done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
+let random = Math.floor(Math.random() * 10) + 1; 
 
-// Write your code here
+let num3 = 25
+let num4 = 7
+let mod = Math.round(num3/num4)
+let x = (1,2,3,7,11,13,17,20)
+let max = Math.max(x)
